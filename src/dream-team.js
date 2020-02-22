9b1e1d2d51c7174034aab2c17f241e6a5e62f769
unknown <yury971@mail.ru>
2020-02-22 11:30:20 +0300

    feat: add solution

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,4 +1,15 @@
-module.exports = function createDreamTeam(/* members */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  var teamtag = [];
+
+  if (Array.isArray(members) != true) {
+    return false;
+  }
+
+  for (var i = 0; i < members.length; i++) {
+    if (typeof members[i] == 'string') {
+      teamtag.push(members[i].trim()[0].toUpperCase());
+    }
+  }
+
+  return teamtag.sort().join('');
 };
