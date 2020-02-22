a56028eaaf4135cd255466df941d95afb44bfe34
unknown <yury971@mail.ru>
2020-02-22 19:44:40 +0300

    feat: add solution

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,5 +1,38 @@
-module.exports = function repeater(/* str, options */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+module.exports = function repeater(str, options) {
+
+    if (options.separator === undefined) {
+        options.separator = '+';
+    }
+    if (options.additionSeparator === undefined) {
+        options.additionSeparator = '|';
+    }
+    if (options.repeatTimes === undefined) {
+        options.repeatTimes = 1;
+    }
+    if (options.additionRepeatTimes === undefined) {
+        options.additionRepeatTimes = 1;
+    }
+    if (options.addition === undefined) {
+        options.additionRepeatTimes = '';
+    }
+
+    var result = '';
+
+    for (var i = 0; i < options.repeatTimes; i++) {
+        result += str;
+
+        for (var j = 0; j < options.additionRepeatTimes; j++) {
+            result += options.addition;
+
+            if (j < options.additionRepeatTimes - 1) {
+                result += options.additionSeparator;
+            }
+        }
+
+        if (i < options.repeatTimes - 1) {
+            result += options.separator;
+        }
+    }
+
+    return result;
 };
-  
