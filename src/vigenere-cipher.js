f1a2a4454d87701ee279f4794fee6c5c4630cdb2
unknown <yury971@mail.ru>
2020-02-23 14:24:50 +0300

    feat: add solution

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,12 +1,46 @@
 class VigenereCipheringMachine {
-    encrypt() {
-        throw 'Not implemented';
-        // remove line with error and write your code here
+
+    constructor(direct = true) {
+        this.direct = direct;
     }
 
-    decrypt() {
-        throw 'Not implemented';
-        // remove line with error and write your code here
+    encrypt(string, key) {
+        return this.crypt(string, key, false);
+    }
+
+    decrypt(string, key) {
+        return this.crypt(string, key, true);
+    }
+
+    crypt(string, key, decryption) {
+        if (string === undefined || key === undefined) {
+            throw Error();
+        }
+
+        string = string.toUpperCase();
+        key = key.toUpperCase();
+
+        const result = [];
+        var encryption = 0;
+        
+        for (var i = 0; i < string.length; ++i) {
+            if (string[i].match(/[A-Z]/)) {
+                if (decryption) {
+                    result.push(String.fromCharCode((string[i].charCodeAt(0) - 'A'.charCodeAt(0) + (26 - (key[encryption % key.length].charCodeAt(0) - 'A'.charCodeAt(0)))) % 26 + 'A'.charCodeAt(0)));
+                } else {
+                    result.push(String.fromCharCode((string[i].charCodeAt(0) - 'A'.charCodeAt(0) + (key[encryption % key.length].charCodeAt(0) - 'A'.charCodeAt(0))) % 26 + 'A'.charCodeAt(0)));
+                }
+                encryption++;
+            } else {
+                result.push(string[i]);
+            }
+        }
+
+        if (!this.direct) {
+            result.reverse();
+        }
+
+        return result.join('');
     }
 }
 
